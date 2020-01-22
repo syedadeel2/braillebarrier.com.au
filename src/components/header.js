f6b59fab2e8077e9bec03810410c4461508b3318
Luke Bennett <hello@lukebennett.com.au>
2020-01-22 15:16:04 +1100

    Style header

--- src/components/header.js
+++ src/components/header.js
@@ -1,66 +1,48 @@
-import React, { useState } from 'react';
-import ReactModal from 'react-modal';
-import { Link, navigate } from 'gatsby';
-import PropTypes from 'prop-types';
+import React from 'react';
+import { Link } from 'gatsby';
 
-const Header = ({ siteTitle }) => {
-  const [isOpen, toggleOpen] = useState(false);
+import useGraphql from '../hooks/use-graphql';
+import siteNavigation from '../data/site-navigation';
+
+const Header = () => {
+  const { site } = useGraphql();
   return (
-    <header className="w-screen mb-6 text-white bg-teal-600">
-      <div className="flex items-center justify-between max-w-4xl px-4 py-6 mx-auto">
-        <h1 className="text-4xl font-bold">
-          <Link to="/">{siteTitle}</Link>
-        </h1>
-        <button
-          onClick={() => toggleOpen(!isOpen)}
-          type="button"
-          className="relative z-50 inline-block px-3 py-2 text-sm font-semibold leading-none tracking-wide uppercase border border-white"
-        >
-          Menu
-        </button>
-        <ReactModal
-          isOpen={isOpen}
-          contentLabel="Site navigation"
-          className="inset-0 flex items-center justify-center h-full text-3xl text-white bg-teal-600"
-        >
-          <nav>
-            <ul>
-              <li>
-                <button
-                  onClick={() => {
-                    toggleOpen(false);
-                    navigate('/');
-                  }}
-                  type="button"
-                >
-                  Home
-                </button>
-              </li>
-              <li>
-                <button
-                  onClick={() => {
-                    toggleOpen(false);
-                    navigate('/page-2');
-                  }}
-                  type="button"
-                >
-                  Page 2
-                </button>
-              </li>
-            </ul>
-          </nav>
-        </ReactModal>
-      </div>
+    <header
+      style={{ background: `linear-gradient(90deg, #0b5eb4 0%, #27aae1 100%)` }}
+      className="w-full h-screen max-w-xs px-12 py-24 overflow-y-auto font-medium text-white bg-blue-600"
+    >
+      <nav>
+        <ul className="w-40 leading-tight uppercase">
+          {siteNavigation.map(navItem => (
+            <li key={navItem.id} className="flex">
+              <Link to={navItem.href} className="pt-4">
+                {navItem.label}
+              </Link>
+            </li>
+          ))}
+        </ul>
+        <hr className="w-8 mt-8 border-t-4 border-white" />
+        <div className="w-40 leading-tight">
+          <p className="mt-4">{site.siteMetadata.address}</p>
+          <p className="mt-1">
+            <small>(Just 3 hours from Sydney)</small>
+          </p>
+          <p>Barrier Signs Pty. Ltd.</p>
+          <p className="mt-4">ABN: {site.siteMetadata.ABN}</p>
+          <p className="mt-4">
+            <a href={`tel:${site.siteMetadata.phone.replace(/\s+/g, '')}`}>
+              {site.siteMetadata.phone}
+            </a>
+          </p>
+          <p className="mt-4">
+            <a href={`mailto:${site.siteMetadata.email}`}>
+              {site.siteMetadata.email}
+            </a>
+          </p>
+        </div>
+      </nav>
     </header>
   );
-};
-
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-};
-
-Header.defaultProps = {
-  siteTitle: ``,
 };
 
 export default Header;
