b86467ee2014b74959b73bc4c3b4ac387b98b9f0
Luke Bennett <hello@lukebennett.com.au>
2020-01-23 09:59:03 +1100

    Add extra spacing utilities

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,10 @@ module.exports = {
       fontFamily: {
         sans: ['Inter', ...tw.fontFamily.sans],
       },
+      spacing: {
+        '72': '18rem',
+        '80': '20rem',
+      },
     },
   },
   variants: {},
