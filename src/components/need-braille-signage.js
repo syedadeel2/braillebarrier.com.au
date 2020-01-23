8fa629269f9860944d628b269a24435cd43f59d8
Luke Bennett <hello@lukebennett.com.au>
2020-01-23 16:25:53 +1100

    Add new section

---
+++ src/components/need-braille-signage.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const NeedBrailleSignage = () => {
+  return (
+    <article
+      id="need-braille-signage"
+      className="px-6 py-12 text-white bg-gradient"
+    >
+      <div className="w-full max-w-3xl mx-auto">
+        <div className="pt-6">
+          <h2 className="mt-12 text-white h2">
+            Need <span className="font-bold">specialise</span> in{' '}
+            <span className="font-bold">Braille</span> and{' '}
+            <span className="font-bold">Tactile Signage</span>.
+          </h2>
+          <a href="/" className="w-auto mt-12 text-blue-600 bg-white button">
+            View our Products here
+          </a>
+        </div>
+      </div>
+    </article>
+  );
+};
+
+export default NeedBrailleSignage;
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import Layout from '../components/layout';
 import SEO from '../components/seo';
 import Home from '../components/home';
 import About from '../components/about';
+import NeedBrailleSignage from '../components/need-braille-signage';
 import Contact from '../components/contact';
 import MeetOurNeighbours from '../components/meet-our-neighbours';
 
@@ -12,6 +13,7 @@ const IndexPage = () => (
     <SEO title="Home" />
     <Home />
     <About />
+    <NeedBrailleSignage />
     <Contact />
     <MeetOurNeighbours />
   </Layout>
