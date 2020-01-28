1410dfe58668efeb3b9a1376c0045c92d8198955
Luke Bennett <hello@lukebennett.com.au>
2020-01-28 22:37:13 +1100

    Add `isActive` key

--- src/data/site-navigation.js
+++ src/data/site-navigation.js
@@ -3,26 +3,31 @@ const siteNavigation = [
     id: 1,
     label: 'Home',
     href: '#home',
+    isActive: true,
   },
   {
     id: 2,
     label: 'About',
     href: '#about',
+    isActive: false,
   },
   {
     id: 3,
     label: 'Products',
     href: '#products',
+    isActive: false,
   },
   {
     id: 4,
     label: 'Contact',
     href: '#contact',
+    isActive: false,
   },
   {
     id: 5,
     label: 'Meet Our Neighbours',
     href: '#meet-our-neighbours',
+    isActive: false,
   },
 ];
 
