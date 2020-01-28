6cd4edaa58b5d83ee61a3e3d6de7a15d7519af5c
Luke Bennett <hello@lukebennett.com.au>
2020-01-28 22:59:35 +1100

    Save progress

--- src/components/home.js
+++ src/components/home.js
@@ -1,45 +1,35 @@
 import React from 'react';
-import { InView } from 'react-intersection-observer';
 import Image from 'gatsby-image';
 
 import TopBar from './topbar';
-import SiteContext from '../context/provider';
 import useGraphql from '../hooks/use-graphql';
 
 const Home = () => {
-  const { navItems, setNavItems } = React.useContext(SiteContext);
   const { homeImage } = useGraphql();
+
   return (
-    <InView>
-      {({ inView, ref }) => {
-        return (
-          <article ref={ref} id="home" className="px-6 py-12">
-            <div className="w-full max-w-3xl pt-6 mx-auto">
-              <TopBar />
-              <div className="pt-6">
-                <h2 className="mt-12 h2">
-                  We create{' '}
-                  <span className="font-semibold">custom Braille</span> and{' '}
-                  <span className="font-semibold">Tactile Signage</span>.
-                </h2>
-                <div className="mt-12 lg:flex">
-                  <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Quia, officiis impedit cum repudiandae dolore doloremque
-                    ducimus aspernatur suscipit enim unde excepturi labore
-                    delectus! Similique suscipit temporibus officiis dolorum
-                    delectus ipsa.
-                  </p>
-                  <div className="relative z-10 flex-shrink-0 mt-12 -mb-32 w-80 lg:mt-0 lg:ml-6">
-                    <Image fluid={homeImage.childImageSharp.fluid} />
-                  </div>
-                </div>
-              </div>
+    <article id="home" className="px-6 py-12">
+      <div className="w-full max-w-3xl pt-6 mx-auto">
+        <TopBar />
+        <div className="pt-6">
+          <h2 className="mt-12 h2">
+            We create <span className="font-semibold">custom Braille</span> and{' '}
+            <span className="font-semibold">Tactile Signage</span>.
+          </h2>
+          <div className="mt-12 lg:flex">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia,
+              officiis impedit cum repudiandae dolore doloremque ducimus
+              aspernatur suscipit enim unde excepturi labore delectus! Similique
+              suscipit temporibus officiis dolorum delectus ipsa.
+            </p>
+            <div className="relative z-10 flex-shrink-0 mt-12 -mb-32 w-80 lg:mt-0 lg:ml-6">
+              <Image fluid={homeImage.childImageSharp.fluid} />
             </div>
-          </article>
-        );
-      }}
-    </InView>
+          </div>
+        </div>
+      </div>
+    </article>
   );
 };
 
--- src/components/products.js
+++ src/components/products.js
@@ -1,56 +1,40 @@
 import React from 'react';
-import { InView } from 'react-intersection-observer';
 import Image from 'gatsby-image';
 
-// import SiteContext from '../context/provider';
 import useGraphql from '../hooks/use-graphql';
 import pointer from '../images/pointer-black.svg';
 
 const Products = () => {
-  // const { navItems, setNavItems } = React.useContext(SiteContext);
   const { productsImage } = useGraphql();
   return (
-    <InView>
-      {({ inView, ref }) => {
-        // const active = navItems.map(navItem =>
-        //   navItem.label === 'Products'
-        //     ? (navItem.isActive = true)
-        //     : (navItem.isActive = false)
-        // );
-        // if (inView) setNavItems(...navItems, active);
-        return (
-          <article
-            ref={ref}
-            id="products"
-            className="relative px-6 py-12 text-white bg-gradient"
-          >
-            <div className="absolute inset-x-0 top-0 flex justify-center transform -translate-y-1/2">
-              <img src={pointer} alt="" className="h-12" />
+    <article
+      id="products"
+      className="relative px-6 py-12 text-white bg-gradient"
+    >
+      <div className="absolute inset-x-0 top-0 flex justify-center transform -translate-y-1/2">
+        <img src={pointer} alt="" className="h-12" />
+      </div>
+      <div className="w-full max-w-3xl pt-6 mx-auto">
+        <div className="lg:flex">
+          <div>
+            <h2 className="mt-12 text-white h2">
+              Need <span className="font-semibold">Braille Signage?</span>
+            </h2>
+            <a
+              href="/resources/information-booklet.pdf"
+              className="w-auto mt-12 text-blue-600 bg-white button"
+            >
+              View our Products here
+            </a>
+          </div>
+          <div className="relative flex-shrink-0 hidden w-40 lg:block lg:ml-6">
+            <div className="w-full lg:absolute lg:inset-0 lg:-mt-48">
+              <Image fluid={productsImage.childImageSharp.fluid} />
             </div>
-            <div className="w-full max-w-3xl pt-6 mx-auto">
-              <div className="lg:flex">
-                <div>
-                  <h2 className="mt-12 text-white h2">
-                    Need <span className="font-semibold">Braille Signage?</span>
-                  </h2>
-                  <a
-                    href="/resources/information-booklet.pdf"
-                    className="w-auto mt-12 text-blue-600 bg-white button"
-                  >
-                    View our Products here
-                  </a>
-                </div>
-                <div className="relative flex-shrink-0 hidden w-40 lg:block lg:ml-6">
-                  <div className="w-full lg:absolute lg:inset-0 lg:-mt-48">
-                    <Image fluid={productsImage.childImageSharp.fluid} />
-                  </div>
-                </div>
-              </div>
-            </div>
-          </article>
-        );
-      }}
-    </InView>
+          </div>
+        </div>
+      </div>
+    </article>
   );
 };
 
