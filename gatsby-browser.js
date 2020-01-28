eaa5e4dc52dd9c491f945372465b0800afaff0c8
Luke Bennett <hello@lukebennett.com.au>
2020-01-28 22:36:57 +1100

    Set up React Context

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,9 +3,18 @@
  *
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
-
 // Self host fonts using Typefaces.js https://github.com/KyleAMathews/typefaces
 import 'typeface-inter';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-// Load CSS
+import { SiteProvider } from './src/context/provider';
 import './src/css/tailwind.css';
+
+export const wrapRootElement = ({ element }) => (
+  <SiteProvider>{element}</SiteProvider>
+);
+
+wrapRootElement.propTypes = {
+  element: PropTypes.node,
+};
